8211ba22b51f49c3ee0f4ea89617b3d8345cdbe8
Abdelilah <you@example.com>
2025-11-01 23:26:24 +0100

    fix import

--- src/features/orderSlice.jsx
+++ src/features/orderSlice.jsx
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axiosInstance from "../api/axios";
+import axiosInstance from "../API/axios";
 
 export const fetchOrders = createAsyncThunk(
     "commandes/fetchCommandes",
